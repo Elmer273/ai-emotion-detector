f496c51df195c007a3dc51f2b5e7831d0b7ac14b
Elmer273 <elmermartinez666@yahoo.com>
2023-10-05 01:17:54 -0400

    Add error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,14 +6,26 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=input_json, headers=header)
-    formatted_response = json.loads(response.text)
-    emotions = {
-        'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
-        'disgust': formatted_response['emotionPredictions'][0]['emotion']['disgust'],
-        'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
-        'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
-        'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness']
-    }
-    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = {
+            'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
+            'disgust': formatted_response['emotionPredictions'][0]['emotion']['disgust'],
+            'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
+            'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
+            'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        }
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    
+    elif response.status_code == 400:
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        } 
     
     return emotions
